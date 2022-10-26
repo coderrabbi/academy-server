eac479ad2f8700e5795ad11eae9ca5df03c826e7
NAYEEM65 <mathnayeem054@gmail.com>
2022-10-26 11:54:41 +0600

    courses routed added

--- index.js
+++ index.js
@@ -5,34 +5,20 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 const categories = require('./data/categories.json');
-const news = require('./data/news.json');
+const courses = require('./data/courses.json');
 
 app.get('/', (req, res) => {
-    res.send('News API Running');
+    res.send('Courses API is Running');
 });
 
-app.get('/news-categories', (req, res) => {
-    res.send(categories);
+app.get('/courses', (req, res) => {
+    res.send(courses);
 });
 
-app.get('/category/:id', (req, res) => {
+app.get('/courses/:id', (req, res) => {
     const id = req.params.id;
-    if (id === '08') {
-        res.send(news);
-    } else {
-        const category_news = news.filter((n) => n.category_id === id);
-        res.send(category_news);
-    }
-});
-
-app.get('/news', (req, res) => {
-    res.send(news);
-});
-
-app.get('/news/:id', (req, res) => {
-    const id = req.params.id;
-    const selectedNews = news.find((n) => n._id === id);
-    res.send(selectedNews);
+    const selectedCourses = courses.find((c) => c.id === id);
+    res.send(selectedCourses);
 });
 
 app.listen(port, () => {
